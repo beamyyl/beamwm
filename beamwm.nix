0c702f5507471f97493437c807e26b2974fb26d9
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-05-31 13:38:06 +0300

    Add files via upload

---
+++ beamwm.nix
@@ -0,0 +1,26 @@
+{ lib, stdenv, libX11, libXft, libxinerama, xorg, freetype, fontconfig, pkg-config, gnumake }:
+
+stdenv.mkDerivation rec {
+  pname = "beamwm";
+  version = "0.1.0";
+  src = /home/beamy/beamwm;
+  nativeBuildInputs = [ pkg-config gnumake ];
+  buildInputs = [ 
+    libX11 
+    libXft 
+    libxinerama
+    fontconfig 
+    freetype 
+  ];
+
+  patchPhase = ''
+    substituteInPlace Makefile \
+      --replace "-I/usr/include/freetype2" "$(pkg-config --cflags freetype2)"
+  '';
+  makeFlags = [ "PREFIX=$(out)" ];
+  passthru.providedSessions = [ "beamwm" ];
+  meta = with lib; {
+    description = "Minimalist wm";
+    platforms = platforms.linux;
+  };
+}
